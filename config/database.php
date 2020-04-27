1adbfa27bdf8b5e7f5b30a8f455924d27f8c8077
Nainoor <nainoorpatel@gmail.com>
2020-04-27 19:25:31 +0530

    Update database.php

--- config/database.php
+++ config/database.php
@@ -54,10 +54,11 @@ return [
 
         'mysql' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'forge'),
-            'username'  => env('DB_USERNAME', 'forge'),
-            'password'  => env('DB_PASSWORD', ''),
+            'host'      => env('DB_HOST', 'db4free.net'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_DATABASE', 'eyedsdb'),
+            'username'  => env('DB_USERNAME', 'eyeds'),
+            'password'  => env('DB_PASSWORD', 'eyeds@321'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
